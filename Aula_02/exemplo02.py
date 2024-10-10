4e1fb3c675b560c41b27b66ebdc8cfd382002386
ICARO GABRIEL PEREIRA CARVALHO <20241014040011@ifrn.local>
2024-10-10 07:50:49 -0300

    Enviado

---
+++ Aula_02/exemplo02.py
@@ -0,0 +1,16 @@
+x = 5
+y = 5.5
+c = False
+s = "TADS - Tecnologia em Analise e Desenvolvimento de Sistemas"
+l = s.split()
+
+print(x, type(x))
+print(y, type(y))
+print(c, type(c))
+print(s, type(s))
+print(l, type(l))
+
+l.sort()
+
+print(l)
+print(type(print))
