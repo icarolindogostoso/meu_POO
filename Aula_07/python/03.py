c9685d515d54550746acb3964a5ca56edd174928
ICARO GABRIEL PEREIRA CARVALHO <20241014040011@ifrn.local>
2024-11-01 11:23:48 -0300

    mensagme

--- Aula_07/python/03.py
+++ Aula_07/python/03.py
@@ -46,16 +46,59 @@ class Cliente:
         
     def setSocio (self, cliente):
         self.__socio = cliente
+        self.__limite = self.__limite + cliente.__limite
 
     def getLimite (self):
         return self.__limite
     
     def __str__ (self):
-        socio = f"e socio = {self.__socio}" if (socio != None) else ""
+        socio = f"e socio = {self.__socio}" if (self.__socio != None) else ""
         return f"Cliente {self.__nome}, com cpf {self.__cpf} {socio}"
     
 
 class UI:
     @staticmethod
     def menu():
-        print("1 - Nova empresa, 2 - Cadastrar cliente, 3 - Listar clientes, 4 -")
+        print("1 - Nova empresa, 2 - Cadastrar cliente, 3 - Listar clientes, 9 - Fim")
+        return int(input("Informe uma opcao: "))
+
+    @staticmethod
+    def main():
+        op = 0
+        empresa = None
+        while op != 9:
+            op = UI.menu()
+            if op == 1:
+                empresa = UI.nova_empresa()
+            if op == 2:
+                if empresa == None:
+                    print("Nenhuma empresa criada!")
+                else:
+                    UI.inserir_cliente(empresa)
+            if op == 3:
+                if empresa == None:
+                    print("Nenhuma empresa criada!")
+                else:
+                    UI.listar_clientes(empresa)
+
+    @staticmethod
+    def nova_empresa():
+        nome = input("Informe o nome da empresa: ")
+        empresa = Empresa(nome)
+        return empresa
+
+    @staticmethod
+    def inserir_cliente (empresa):
+        nome = input("Informe o nome do cliente: ")
+        cpf = input("Informe o cpf do cliente: ")
+        limite = float(input("Informe o limite do cliente: "))
+        cliente = Cliente(nome,cpf,limite)
+        empresa.inserir(cliente)
+
+    @staticmethod
+    def listar_clientes(empresa):
+        print(empresa)
+        for cliente in empresa.listar():
+            print(cliente)
+
+UI.main()
