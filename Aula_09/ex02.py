4a7a8a38b6f8fc733b58b02ea36136e9828e26b2
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2024-11-07 13:27:21 +0000

    mensagem

--- Aula_09/ex02.py
+++ Aula_09/ex02.py
@@ -1,3 +1,5 @@
+import json
+
 class Cliente:
     def __init__ (self, id, nome):
         self.id = id
@@ -6,14 +8,32 @@ class Cliente:
     def __str__ (self):
         return f"{self.id} - {self.nome}"
 
-a = Cliente(1, "Icaro")
-b = Cliente(2, "Gustavo")
+def salvar():
+    a = Cliente(1, "Icaro")
+    b = Cliente(2, "Gustavo")
 
-print(a)
-print(b)
+    print(a)
+    print(b)
 
-print(a.__dict__)
-print(b.__dict__)
+    print(a.__dict__)
+    print(b.__dict__)
 
-print(vars(a))
-print(vars(b))
+    print(vars(a))
+    print(vars(b))
+
+    clientes = [a, b]
+    with open("Aula_09/clientes.json", mode="w") as arquivo:
+        json.dump(clientes, arquivo, default = vars)
+
+def abrir():
+    clientes = []
+    with open("Aula_09/clientes.json", mode="r") as arquivo:
+        clientes_json = json.load(arquivo)
+        for obj in clientes_json:
+            c = Cliente(obj["id"], obj["nome"])
+            clientes.append(c)
+
+    for c in clientes:
+        print(c)
+
+abrir()
