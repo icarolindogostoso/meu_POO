c93368add803ab1a7ff9fa0d8c7959f04d472af1
ICARO GABRIEL PEREIRA CARVALHO <20241014040011@ifrn.local>
2024-12-17 11:24:05 -0300

    mensagem

--- Aula_14/templates/visualizarmeuspedidosUI.py
+++ Aula_14/templates/visualizarmeuspedidosUI.py
@@ -22,8 +22,18 @@ class VisualizarMeusPedidosUI:
             st.write("Nenhum pedido realizado")
         else:
             for vendaitem in View.vendaItemListar():
+                produto = None
+                for obj in View.produtoListar():
+                    if obj.getId() == vendaitem.getIdProduto():
+                        produto = obj
+                        break
+
                 if vendaitem.getIdVenda() == venda.getId():
                     with st.container(border=True):
-                        st.header(f"Produto: {vendaitem.getNome()}")
-                        st.write(f"Preço: R$ {vendaitem.getPreco():.2f}")
-                        st.write(f"Quantidade: {vendaitem.getQtd()}")
+                        col1, col2 = st.columns(2)
+                        with col1:
+                            st.image(produto.getFoto(), width=500)
+                        with col2:
+                            st.header(f"Produto: {vendaitem.getNome()}")
+                            st.write(f"Preço: R$ {vendaitem.getPreco():.2f}")
+                            st.write(f"Quantidade: {vendaitem.getQtd()}")
